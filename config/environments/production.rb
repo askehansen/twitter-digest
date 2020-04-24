950c1cf3fa53d92bec03bbc7ee38c3ad03e16223
Aske Hansen <aske@deeco.dk>
2020-04-24 15:58:27 +0800

    elastic emailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,11 +105,11 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'twitter-digest.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['ELASTIC_EMAIL_USERNAME'],
+    :password => ENV['ELATIC_EMAIL_PASSWORD'],
     :domain => 'twitter-digest.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
+    :address => 'smtp.elasticemail.com',
+    :port => 2525,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
