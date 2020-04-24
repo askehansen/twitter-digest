918d37b91a9fb38a36ff6e1918db445feb3910c6
Aske Hansen <aske@deeco.dk>
2020-04-24 15:09:20 +0800

    disable purge

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -15,29 +15,29 @@ const glob = require('glob-all')
 
 
 
-// ensure classes with special chars like -mt-1 and md:w-1/3 are included
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-z0-9-:/]+/g);
-  }
-}
-
-environment.plugins.append('PurgecssPlugin', new PurgecssPlugin({
-  paths: glob.sync([
-    path.join(__dirname, '../../app/views/**/*.slim'),
-    path.join(__dirname, '../../app/views/**/*.erb'),
-    path.join(__dirname, '../../app/decorators/**/*.rb'),
-    path.join(__dirname, '../../app/helpers/**/*.rb'),
-    path.join(__dirname, '../../app/javascript/**/*.js'),
-    path.join(__dirname, '../../app/assets/javascripts/**/*.js'),
-  ]),
-  extractors: [ // if using Tailwind
-    {
-      extractor: TailwindExtractor,
-      extensions: ['slim', 'js', 'erb', 'rb']
-    }
-  ]
-}));
+// // ensure classes with special chars like -mt-1 and md:w-1/3 are included
+// class TailwindExtractor {
+//   static extract(content) {
+//     return content.match(/[A-z0-9-:/]+/g);
+//   }
+// }
+//
+// environment.plugins.append('PurgecssPlugin', new PurgecssPlugin({
+//   paths: glob.sync([
+//     path.join(__dirname, '../../app/views/**/*.slim'),
+//     path.join(__dirname, '../../app/views/**/*.erb'),
+//     path.join(__dirname, '../../app/decorators/**/*.rb'),
+//     path.join(__dirname, '../../app/helpers/**/*.rb'),
+//     path.join(__dirname, '../../app/javascript/**/*.js'),
+//     path.join(__dirname, '../../app/assets/javascripts/**/*.js'),
+//   ]),
+//   extractors: [ // if using Tailwind
+//     {
+//       extractor: TailwindExtractor,
+//       extensions: ['slim', 'js', 'erb', 'rb']
+//     }
+//   ]
+// }));
 
 
 
