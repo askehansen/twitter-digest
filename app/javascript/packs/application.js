b7c4e40fa643db280b02e80f33b4cef798f7bc1f
Aske Hansen <aske@deeco.dk>
2020-04-27 13:59:44 +0800

    add animation to new digest

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,10 +15,12 @@ const application = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
+import '@lottiefiles/lottie-player'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
