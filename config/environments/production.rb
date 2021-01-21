9d9bb2e47e6a627aecc4858132899d3297735cd9
Aske Hansen <aske@deeco.dk>
2021-01-21 13:48:12 +0100

    -

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://twitter-digest.herokuapp.com/' ]
+  config.action_cable.allowed_request_origins = [ 'https://twitter-digest.deeco.dk/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
